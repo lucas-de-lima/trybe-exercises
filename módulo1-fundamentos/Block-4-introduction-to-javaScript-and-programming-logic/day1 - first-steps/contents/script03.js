323a2e1ea1452207beb1f8aa366da2a7e879dfc4
Lucas-De-Lima <lucasramosdelima@gmail.com>
2022-07-26 10:36:46 -0300

    atualizando if/else

--- módulo1-fundamentos/Block-4-introduction-to-javaScript-and-programming-logic/day1 - first-steps/contents/script03.js
+++ módulo1-fundamentos/Block-4-introduction-to-javaScript-and-programming-logic/day1 - first-steps/contents/script03.js
@@ -1,13 +1,13 @@
 const lucas = 80;
 
-if (lucas) > = 80 {
+if (lucas >= 80) {
     console.log("Parabéns, você foi aprovado!");
 }
-else if (lucas) < 80, | = 60 {
+else if (lucas <80, || lucas =60); {
 
         console.log("Você está na nossa lista de espera");
-    }
-else (lucas) < 60 {
+} 
+else (lucas < 60); {
         console.log("Você foi reprovado");
 }
 //esse código não funciona como deve
