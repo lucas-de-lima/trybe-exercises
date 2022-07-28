425ef5caa0259f4bfd27de25a480236caacfcbb9
Lucas-De-Lima <lucasramosdelima@gmail.com>
2022-07-27 23:20:03 -0300

    encerrando o dia

---
+++ módulo1-fundamentos/block5-javascript-DOM-events-e-web-storage/day1/contents/script.js
@@ -0,0 +1,5 @@
+document.getElementById("page-title")
+// document.getElementById("paragraph").innerText = "opa";
+
+console.log(document.getElementsByClassName("paragrafos")[0].innerText = "novo paragrafo");
+document.getElementsByTagName("h2")[0].style =  
