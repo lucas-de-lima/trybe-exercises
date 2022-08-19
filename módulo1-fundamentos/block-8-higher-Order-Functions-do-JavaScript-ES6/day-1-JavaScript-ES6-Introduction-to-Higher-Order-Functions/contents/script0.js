ff41dbb4480ba628913d68b90d56557c2d0f3936
Lucas-De-Lima <lucasramosdelima@gmail.com>
2022-08-19 10:05:18 -0300

    atualizando dia 8.1

--- módulo1-fundamentos/block-8-higher-Order-Functions-do-JavaScript-ES6/day-1-JavaScript-ES6-Introduction-to-Higher-Order-Functions/contents/script0.js
+++ módulo1-fundamentos/block-8-higher-Order-Functions-do-JavaScript-ES6/day-1-JavaScript-ES6-Introduction-to-Higher-Order-Functions/contents/script0.js
@@ -1,3 +1,4 @@
+//First-Class Functions
 const sayHello = () => {
     return ('hello trybers');
   }
---
+++ módulo1-fundamentos/block-8-higher-Order-Functions-do-JavaScript-ES6/day-1-JavaScript-ES6-Introduction-to-Higher-Order-Functions/contents/script02.js
@@ -0,0 +1,50 @@
+// Estruturando uma HOF
+
+// const repeat = (number, action) => {
+//     for (let count = 0; count <= number; count += 1) {
+//       action(count);
+//     }
+//   };
+  
+//   repeat(5, console.log);
+
+// const repeat = (number, action) => {
+//     for (let count = 0; count <= number; count += 1) {
+//       action(count);
+//     }
+//   };
+  
+//   repeat(3, (number) => { // o segundo parametro é uma arrow function
+//     if (number % 2 === 0) {
+//       console.log(number, 'is even');
+//     }
+//   }); //os parenteses dos parametros fecham aqui, que doidera
+
+  //Pense agora que gostaríamos de testar quais números são ímpares. Veja como fica fácil ajustar a implementação:
+
+//   const repeat = (number, action) => {
+//     for (let count = 0; count <= number; count += 1) {
+//       action(count);
+//     }
+//   };
+  
+//   const isEven = (number) => {
+//     if (number % 2 === 0) {
+//       console.log(number, 'is even');
+//     }
+//   };
+  
+//   const isOdd = (number) => {
+//     if ((number % 2) > 0) {
+//       console.log(number, 'is odd');
+//     }
+//   };
+  
+//   repeat(3, isEven); // Testa quais números serão pares;
+//   repeat(3, isOdd); // Testa quais números serão ímpares;
+
+const numberGenerator = () => {
+    return Math.random() * 100;
+  }
+  
+  console.log(numberGenerator);
