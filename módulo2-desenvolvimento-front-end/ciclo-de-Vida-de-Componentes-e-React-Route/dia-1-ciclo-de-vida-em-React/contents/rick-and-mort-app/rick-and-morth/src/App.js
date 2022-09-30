06ca882125139609cf50fcd5c58f5cdf7e842bc3
Lucas-De-Lima <lucasramosdelima@gmail.com>
2022-09-30 09:58:45 -0300

    encerrando dia 4.1

--- módulo2-desenvolvimento-front-end/ciclo-de-Vida-de-Componentes-e-React-Route/dia-1-ciclo-de-vida-em-React/contents/rick-and-mort-app/rick-and-morth/src/App.js
+++ módulo2-desenvolvimento-front-end/ciclo-de-Vida-de-Componentes-e-React-Route/dia-1-ciclo-de-vida-em-React/contents/rick-and-mort-app/rick-and-morth/src/App.js
@@ -1,11 +1,10 @@
+import logo from './logo.svg';
 import './App.css';
-import Counter from './components/Counter';
 import DadJoke from './components/DadJoke';
 
 function App() {
   return (
     <div className="App">
-      <Counter />
       <DadJoke />
     </div>
   );
