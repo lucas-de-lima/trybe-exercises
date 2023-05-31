4f895fb49a9a583b3ddb9030d9119b4a85024d73
Lucas-De-Lima <lucasramosdelima@gmail.com>
2023-05-31 16:34:27 -0300

    feat: add coments on exercises

--- modulo4-ciencia-da-computacao/dia-1/exercises/exercise-5.py
+++ modulo4-ciencia-da-computacao/dia-1/exercises/exercise-5.py
@@ -1,3 +1,7 @@
+# Exercício 5: Considere que a cobertura da tinta é de 1 litro para cada 3 metros quadrados e que a tinta é vendida em latas de 18 litros,
+# que custam R$ 80,00. Crie uma função que retorne dois valores em uma tupla contendo a quantidade de latas de tinta a serem compradas
+# e o preço total a partir do tamanho de uma parede (em m²).
+
 def custo_tinta(area):
     lata_preco = 80
     litros_necessarios = area / 3
--- modulo4-ciencia-da-computacao/dia-1/exercises/exercise-6.py
+++ modulo4-ciencia-da-computacao/dia-1/exercises/exercise-6.py
@@ -1,3 +1,13 @@
+# Exercício 6: Crie uma função que receba os três lado de um triângulo e informe qual o tipo de triângulo formado ou "não é triangulo",
+#  caso não seja possível formar um triângulo.
+
+# 🦜 Dica:
+
+#   Três lados formam um triângulo quando a soma de quaisquer dois lados for maior que o terceiro;
+#   Triângulo Equilátero: três lados iguais;
+#   Triângulo Isósceles: quaisquer dois lados iguais;
+#   Triângulo Escaleno: três lados diferentes.
+
 def tipo_de_triangulo(lado1, lado2, lado3):
         triangulo_valido = (
             lado1 + lado2 > lado3 and
